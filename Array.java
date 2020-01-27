44a4c24a7c063ccda163d5f8fefdd4083ced0b2b
yamrajchaudhary <59572679+yamrajchaudhary@users.noreply.github.com>
2020-01-27 22:26:26 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,28 @@
+package com.cts.training.lib;
+
+public class Array {
+     public void average(int arr[]) {
+    	 {
+    		float avg=0;
+    		int sum=0,len,count=0;
+    		 len=arr.length;
+    		 for(int i=0;i<=len;i++) {
+    			    if (arr[i]%5==0)
+    			    {
+    			    	sum=sum+arr[i];
+    			    	count++;
+    			    	 avg=sum/count;
+    			}
+    		 }
+    	 }
+    	 
+     }
+     
+     
+     public void minposition (int n) {
+    	  int min=arr[];
+    	  Object ;
+	
+      	 
+     }
+}
